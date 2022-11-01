5a33b4f168bfb8b3a69eea9f2a99d7d4cbfd0abf
ducchetrongminh <ducchetrongminh@gmail.com>
2022-11-01 07:58:50 +0700

    windows and macos script

---
+++ bin/macos/terminal_startup.sh
@@ -0,0 +1,7 @@
+source ~/.bashrc
+
+# config cho DBT
+export DBT_PROFILES_DIR=$(pwd)
+
+# bật virtual environment của Python
+source .dbtenv/bin/activate
---
+++ bin/windows/terminal_startup.bat
@@ -0,0 +1,5 @@
+:: config cho DBT
+set DBT_PROFILES_DIR=%cd%
+
+:: bật virtual environment của Python
+call .dbtenv\Scripts\activate
